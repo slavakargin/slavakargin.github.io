488443954e4ddafffc440e1262955f341c195ed2
slavakargin <slavakargin@yahoo.com>
2025-10-26 20:14:20 -0400

    Clean root: keep mirror only under /bu

--- assets/b8e4b2aa1ce2.php
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0"?>
-<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
-  <ShortName>Department of Mathematics and Statistics, Binghamton University</ShortName>
-  <Image width="16" height="16" type="image/x-icon">https://www2.math.binghamton.edu/favicon.ico</Image>
-  <Url type="text/html" template="https://www2.math.binghamton.edu/p?do=search&amp;id={searchTerms}" />
-  <Url type="application/x-suggestions+json" template="https://www2.math.binghamton.edu/lib/exe/ajax.php?call=suggestions&amp;q={searchTerms}" />
-</OpenSearchDescription>
